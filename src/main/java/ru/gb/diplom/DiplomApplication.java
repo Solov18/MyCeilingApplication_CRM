dd3a5a30896a5849103994734b58a43e0a0c918c
Solov18 <solov18@list.ru>
2024-05-28 15:01:35 +0300

    добавил зависимости

---
+++ src/main/java/ru/gb/diplom/DiplomApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.diplom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomApplication.class, args);
+	}
+
+}
